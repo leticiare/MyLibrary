03349449b9d84197121d7b58b705ef8d88a117bd
Yasmim <yasmim.lima@dcomp.ufs.br>
2024-02-19 17:14:03 -0300

    Cria tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
+import React from 'react';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
 
-import './App.css'
-import Navbar from './components/Navbar'
 function App() {
-
-
   return (
-   
-    <header>
-      <Navbar    links={[
-            { text: 'Home', href: '/' },
-            { text: 'Sobre', href: '/sobre' },
-            { text: 'Contato', href: '/contato' },
-            { text: 'Ajuda', href: '/ajuda' },
-          ]} />
-    </header>
-
-  )
+    <div>
+      <header>
+        <Navbar links={[
+          { text: 'Home', href: '/' },
+          { text: 'Sobre', href: '/sobre' },
+          { text: 'Contato', href: '/contato' },
+          { text: 'Ajuda', href: '/ajuda' },
+        ]} />
+      </header>
+      <main>
+        <Login />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"),
+  require('@tailwindcss/forms')],
+
   daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
